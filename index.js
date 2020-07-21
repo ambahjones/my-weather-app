710c43a00b7b731a9bb5d08fa6d49d3530d21355
ambahjones <67397295+ambahjones@users.noreply.github.com>
2020-07-21 17:21:09 -0400

    reformatted date and time and fixed minor issues with ids. added hourly forecast instead of 5 day

--- index.js
+++ index.js
@@ -1,63 +1,54 @@
-let days = [
-  "Sunday",
-  "Monday",
-  "Tuesday",
-  "Wednesday",
-  "Thursday",
-  "Friday",
-  "Saturday",
-];
+function formatDate(timestamp) {
+  let now = new Date(timestamp);
 
-let hours = [
-  "12",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-  "12",
-  "1",
-  "2",
-  "3",
-  "4",
-  "5",
-  "6",
-  "7",
-  "8",
-  "9",
-  "10",
-  "11",
-];
+  let days = [
+    "Sunday",
+    "Monday",
+    "Tuesday",
+    "Wednesday",
+    "Thursday",
+    "Friday",
+    "Saturday",
+  ];
 
-let now = new Date();
-let day = days[now.getDay()];
-let hour = hours[now.getHours()];
-let minutes = now.getMinutes();
-if (minutes < 10) {
-  minutes = `0${minutes}`;
+  let day = days[now.getDay()];
+  return `${day} ${formatHours(timestamp)}`;
 }
 
-let time = document.querySelector("#current-day");
-time.innerHTML = `${day}, ${hour}:${minutes}`;
-
-function searchCity(inputCity) {
-  let apiKey = "093e071e943ff87f9fc6c8107074ad0a";
-  let apiSource = "api.openweathermap.org/data/2.5/weather?q=";
-  let apiUrl = `https://${apiSource}${inputCity}&appid=${apiKey}&units=imperial`;
-
-  axios.get(apiUrl).then(displayLocationTemp);
-}
-
-function handleSubmit(event) {
-  event.preventDefault();
-  let inputCity = document.querySelector("#city-to-search").value;
-  searchCity(inputCity);
+function formatHours(timestamp) {
+  let hours = [
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+    "12",
+    "1",
+    "2",
+    "3",
+    "4",
+    "5",
+    "6",
+    "7",
+    "8",
+    "9",
+    "10",
+    "11",
+  ];
+  let now = new Date(timestamp);
+  let hour = hours[now.getHours()];
+  let minutes = now.getMinutes();
+  if (minutes < 10) {
+    minutes = `0${minutes}`;
+  }
+  return `${hour}:${minutes}`;
 }
 
 function displayLocationTemp(response) {
@@ -65,12 +56,15 @@ function displayLocationTemp(response) {
   highTemp = response.data.main.temp_max;
   lowTemp = response.data.main.temp_min;
   speedImperial = response.data.wind.speed;
+  document.querySelector("#current-day").innerHTML = formatDate(
+    response.data.dt * 1000
+  );
   document.querySelector("#current-city").innerHTML = response.data.name;
   document.querySelector("#emoji-description").innerHTML =
     response.data.weather[0].description;
   document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
-  document.querySelector("#highMajor").innerHTML = Math.round(highTemp);
-  document.querySelector("#lowMajor").innerHTML = Math.round(lowTemp);
+  document.querySelector("#high-major").innerHTML = Math.round(highTemp);
+  document.querySelector("#low-major").innerHTML = Math.round(lowTemp);
   document.querySelector("#percent").innerHTML = Math.round(
     response.data.main.humidity
   );
@@ -79,11 +73,52 @@ function displayLocationTemp(response) {
     .querySelector("#weather-icon")
     .setAttribute(
       "src",
-      `http://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
+      `https://openweathermap.org/img/wn/${response.data.weather[0].icon}@2x.png`
     );
   document
     .querySelector("#weather-icon")
-    .setAttribute("alt", `http://openweathermap.org/img/wn/02d@2x.png`);
+    .setAttribute("alt", `https://openweathermap.org/img/wn/02d@2x.png`);
+}
+
+function displayForecast(response) {
+  let showForecast = document.querySelector("#forecast");
+  showForecast.innerHTML = null;
+  let forecast = null;
+  for (let index = 0; index < 6; index++) {
+    forecast = response.data.list[index];
+    showForecast.innerHTML += `
+    <div class="col-2">
+      <h3 id="forecast-time">${formatHours(forecast.dt * 1000)}</h3>
+      <img src="https://openweathermap.org/img/wn/${
+        forecast.weather[0].icon
+      }@2x.png" id="forecast-icon" />
+      <div id="forecast-temps">
+        <strong>
+          <span id="high-minor">${Math.round(
+            forecast.main.temp_max
+          )}</span>°</strong>/<span id="low-minor">${Math.round(
+      forecast.main.temp_min
+    )}</span>°
+      </div>
+    </div>
+  `;
+  }
+}
+
+function searchCity(inputCity) {
+  let apiKey = "093e071e943ff87f9fc6c8107074ad0a";
+  let apiSource = "api.openweathermap.org/data/2.5/weather?q=";
+  let apiUrl = `https://${apiSource}${inputCity}&appid=${apiKey}&units=imperial`;
+  axios.get(apiUrl).then(displayLocationTemp);
+
+  let url = `https://api.openweathermap.org/data/2.5/forecast?q=${inputCity}&appid=${apiKey}&units=imperial`;
+  axios.get(url).then(displayForecast);
+}
+
+function handleSubmit(event) {
+  event.preventDefault();
+  let inputCity = document.querySelector("#city-to-search");
+  searchCity(inputCity.value);
 }
 
 function getCurrent(position) {
@@ -105,10 +140,10 @@ function displayCelciusTemp(event) {
   document.querySelector("#temperature").innerHTML = Math.round(
     ((fahrenheitTemp - 32) * 5) / 9
   );
-  document.querySelector("#highMajor").innerHTML = Math.round(
+  document.querySelector("#high-major").innerHTML = Math.round(
     ((highTemp - 32) * 5) / 9
   );
-  document.querySelector("#lowMajor").innerHTML = Math.round(
+  document.querySelector("#low-major").innerHTML = Math.round(
     ((lowTemp - 32) * 5) / 9
   );
   document.querySelector("#speed").innerHTML = Math.round(
@@ -120,8 +155,8 @@ function displayCelciusTemp(event) {
 function displayFahrenheitTemp(event) {
   event.preventDefault();
   document.querySelector("#temperature").innerHTML = Math.round(fahrenheitTemp);
-  document.querySelector("#highMajor").innerHTML = Math.round(highTemp);
-  document.querySelector("#lowMajor").innerHTML = Math.round(lowTemp);
+  document.querySelector("#high-major").innerHTML = Math.round(highTemp);
+  document.querySelector("#low-major").innerHTML = Math.round(lowTemp);
   document.querySelector("#speed").innerHTML = Math.round(speedImperial);
   document.querySelector("#speed-unit").innerHTML = "mph";
 }
